89809a2e019ca6e8eaf358c3fa2236f0a14d2ca1
MortenSaabye <saabye93@gmail.com>
2017-10-19 17:51:55 +0200

    removed opponent class

--- quiz-sockets/Model/Opponent.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Opponent.swift
-//  quiz-sockets
-//
-//  Created by Morten Saabye Kristensen on 17/10/2017.
-//  Copyright © 2017 Morten Saabye Kristensen. All rights reserved.
-//
-
-import Foundation
